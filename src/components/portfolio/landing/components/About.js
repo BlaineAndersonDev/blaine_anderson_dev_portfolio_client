5c77a0e732ce2f17efacb5033112486aac34231c
BlaineAndersonDev <BlaineAndersonDev@Gmail.com>
2020-01-03 16:50:34 -0700

    Fixed spelling error

--- src/components/portfolio/landing/components/About.js
+++ src/components/portfolio/landing/components/About.js
@@ -12,7 +12,7 @@ class About extends Component {
 
         <div id="aboutBotBox" className="aboutBox">
           <p>A few years ago I began my journey as a freelance developer and since then I've done remote work for agencies, consulted for startups, and collaborated with talented people to create digital products for both business and consumer use.</p>
-          <p>I'm a confidant leader, an attentive team member, a critical thinker, and constantly striving to improve my abilities one project at a time.</p>
+          <p>I'm a confident leader, an attentive team member, a critical thinker, and constantly striving to improve my abilities one project at a time.</p>
         </div>
 
       </div>
